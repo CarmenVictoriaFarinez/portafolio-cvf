2f22f2a3747271a7c9d8a6f3a1f78835ea4ed415
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-09-10 22:26:20 -0500

    cambiando url project bq api cliente

--- src/components/MisProyectos.js
+++ src/components/MisProyectos.js
@@ -80,7 +80,7 @@ const classes = useStyles();
                 </CardContent>
                 </CardActionArea>
                 <CardActions>
-                    <Button target="_blank" href="https://carmenvictoriafarinez.github.io/LIM012-fe-burger-queen-api-client/"
+                    <Button target="_blank" href="https://lim012burgerqueenapiclient.web.app/"
                     style={{ background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}>
                         Website
                     </Button>
